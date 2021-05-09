7c8b90941b8f0f436658259336278a907941c557
MaksimDemin <mdeminalex@icloud.com>
2021-05-09 23:45:02 +0200

    Initial Commit

---
+++ Module8-HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module8-HW
+//
+//  Created by Maksim Demin on 09.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
